dc5b29b98825d7aac8ffa69baa34270cadff1c35
thinker-g <jiyan.guo@gmail.com>
2016-02-06 23:24:51 +0800

    Add missed functions in interface Oauth2Account.

--- interfaces/Oauth2Account.php
+++ interfaces/Oauth2Account.php
@@ -6,6 +6,8 @@ interface Oauth2Account
     public static function findByOpenUid($openUid, $from_source);
     public static function findByUserId($userId, $from_source);
     public function getOpenUid();
+    public function getUserId();
+    public function setUserId($userId);
 }
 
 ?>
